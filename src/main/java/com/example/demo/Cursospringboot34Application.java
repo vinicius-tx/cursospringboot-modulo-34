8f61d03881a9cc1389951e2ef497faa68a19e15a
Marcus Vinicius Mello Da Silva <vinicius.pro.contact@gmail.com>
2020-06-22 14:02:00 -0300

    Projeto Iniciado

---
+++ src/main/java/com/example/demo/Cursospringboot34Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursospringboot34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursospringboot34Application.class, args);
+	}
+
+}
