ef96200ec273bd16923a027dd4c4bd0833f946e5
Marcus Vinicius Mello Da Silva <vinicius.pro.contact@gmail.com>
2020-06-22 15:02:58 -0300

    Acessando a pagina cadastropessoa através de index e pessoacontroller

--- src/main/java/com/example/demo/Cursospringboot34Application.java
+++ src/main/java/com/example/demo/Cursospringboot34Application.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.example.demo.model")
+@ComponentScan(basePackages = {"com.*"})
 public class Cursospringboot34Application {
 
 	public static void main(String[] args) {
